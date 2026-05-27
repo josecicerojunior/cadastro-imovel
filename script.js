b5ea70f483eab784698b66fe7a0acc5716404323
Jose Cicero Junior <juniorgranjeiro.jc@gmail.com>
2026-05-27 10:04:11 -0300

    correção do item

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ opcao = prompt(
 "Bem vindo ao cadastro de Imóveis Plínio Jaime!\n" +
 "Total de imóveis: " + imoveis.length +
 "\n\n Escolha uma opção\n1. Novo Imóvel\n2. Lista imóveis\n3. Sair"
-
 )
+switch(opcao){
+    case "1": 
+    case "2":
+    case "3":
+        alert("Opção invalida!");
 
 }
-while(opcao !== 3)
+
+}
+while(opcao !== "3")
