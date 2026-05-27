7baa64f7d6504854f90cc918843c9b1f4653d917
Jose Cicero Junior <juniorgranjeiro.jc@gmail.com>
2026-05-27 09:46:30 -0300

    criando menu de seleção

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
 const imoveis = []
-let opcao
+
+let opcao 
 
 do {
-    opcao = prompt(
-        "Bem vindo ao cadastro de Imóveis!\n" +
-        "Total de Imóveis: " + imoveis.length +
-        "\n\nEscolha uma opção\n1. Novo imóvel\n2. Lista imóveis\n3. Sair" 
+opcao = prompt(
+"Bem vindo ao cadastro de Imóveis Plínio Jaime!\n" +
+"Total de imóveis: " + imoveis.length +
+"\n\n Escolha uma opção\n1. Novo Imóvel\n2. Lista imóveis\n3. Sair"
 
-    )
+)
 
-    switch(opcao){
-        case "1":
-            const imovel = {}
-            imovel.proprietario = prompt("Informe o proprietário do imóvel:")
-            imovel.quartos = prompt("Quantos quartos possui o imóvel?:")
-            imovel.banheiros = prompt("Quantos banheiros possui o imóvel?:")
-            imovel.garagem = prompt("O imóvel possui garagem?:")
-
-            const confimacao = confirm(
-            "Salvar este imóvel?\n" +
-            "\nProprietario " + imovel.proprietario +
-            "\nQuartos: " + imovel.quartos +
-            "\nBanheiros " + imovel.banheiros +
-            "\nPossui garagem " + imovel.garagem
-            
-            )
-            if (confirmacao){
-                imoveis.push(imovel)
-            }break
-        case "2":
-        case "3":
-            alert("Opção inválida1!");
-    }
-} while(opcao !== "3")
-
+}
+while(opcao !== 3)
